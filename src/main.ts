db3f56f6850e862a3a8ef33d921e219c43d81187
nathan <nathan.ferreiira.dev@gmail.com>
2026-04-13 10:51:42 -0300

    fix: test script

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,4 @@ const customer = new Customer("123", "Nathan");
 const address = new Address("Rua 1", 123, "12345-678", "São Paulo");
 customer.Address = address;
 customer.activate();
-
-const item1 = new OrderItem("1", "Item 1", 10);
-const item2 = new OrderItem("2", "Item 2", 20);
-
-const order = new Order("1", customer._id, [item1, item2]);
-
 console.log("Customer:", customer);
-console.log("Order:", order);
